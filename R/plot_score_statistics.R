3a7cb425c9780b296611f83dfe281b4b8d1f61b0
cdeanj <cdeanjbio@gmail.com>
2023-06-27 18:56:04 -0600

    Add documentation for plot_score_statistics function

--- R/plot_score_statistics.R
+++ R/plot_score_statistics.R
@@ -1,3 +1,17 @@
+#' Plot score statistics output from decontam
+#'
+#' @description
+#' A visualization of the distribution of score statistics output from the decontam package (Davis et al., 2018).  As the program authors note, a visualization of the score statistics is needed to inform a threshold for which to determine if a sequence feature is a contaminant or not.
+#'
+#' @param x A data.frame of classification information output from the isContaminant function in the decontam package.
+#'
+#' @return A ggplot object showing the distribution of score statistics assigned to each sequence feature.
+#' @export
+#'
+#' @examples
+#' physeq <- readRDS(system.file("extdata", "MUClite.rds", package="decontam"))
+#' contam_results <- decontam::isContaminant(physeq, method = 'frequency', conc="quant_reading")
+#' plot_score_statistics(contam_results)
 plot_score_statistics <- function(x) {
   if(nrow(x) == 0) { stop("Data frame is empty") }
   hist(x$p)
